23eb8b3ff8b0836f9cbe058e49f0e4c52797317b
demo <siddharthsingh1071@gmail.com>
2023-06-04 11:06:48 +0530

    CircularLinkedLists commit

---
+++ 3_CircularLinkedLists.py
@@ -0,0 +1,40 @@
+# Circular linked lists Example
+
+class Head:
+    def __init__(self):
+        self.head_pointer = None
+
+    # TRAVERSE FUNCTION
+    def traverse(self):
+        temp_head = self.head_pointer
+        if temp_head == None:
+            print("Node Unavailable")
+        else:
+            while True:
+                print(temp_head.val)
+                temp_head = temp_head.next
+                if temp_head == self.head_pointer:
+                    break
+
+
+
+class Node:
+    def __init__(self,val=None):
+        self.val=val
+        self.next=None
+
+
+head =  Head()
+
+head.head_pointer = Node("Mon")
+n2 = Node("Tue")
+head.head_pointer.next = n2     
+n3 =  Node("Wed")
+n2.next = n3
+n3.next = head.head_pointer
+
+# TRAVERSE FUNCTION
+head.traverse()
+
+
+
